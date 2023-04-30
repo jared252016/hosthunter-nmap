acc189eb8945ad19e8b34b15922f5e9d60af2578
Jared Weisinger <daniel@eanix.net>
2023-04-30 04:09:34 -0500

    Fixed program title

--- hh-nmap.py
+++ hh-nmap.py
@@ -27,7 +27,7 @@ def parse_targets(file):
     
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-                        prog='hh-nmap',
+                        prog='hh-nmap.py',
                         description='Read a txt file from hosthunter and run nmap on them.',
                         epilog='Author: Daniel Weisinger')
 
